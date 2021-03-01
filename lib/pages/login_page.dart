d753d0cbe3ebc725c65caca16b5bdcc0f02b058c
kunal kolhe <kunal.kolhe@webdunia.net>
2021-03-01 19:45:48 +0530

    Form Validation, GlobalKey, Formstate, Day8

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -11,6 +11,23 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String txt ="";
   bool changeButton = false;
+  final _key = GlobalKey<FormState>();
+
+  moveToHomepage() async {
+    if (_key.currentState.validate()) {
+      setState(() {
+        changeButton = true;
+      });
+
+      await Future.delayed(Duration(seconds: 1));
+      Navigator.pushNamed(context, MyRoutes.homeRoute);
+
+      setState(() {
+        changeButton = false;
+      });
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -35,62 +52,74 @@ class _LoginPageState extends State<LoginPage> {
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        hintText: "Please enter username",
-                        labelText: "Username",
-                      ),
-                      onChanged: (value){
-                        txt = value;
-                        setState(() {
-                        });
-                      },
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: "Please enter password",
-                        labelText: "Password",
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
+                child: Form(
+                  key: _key,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Please enter username",
+                          labelText: "Username",
+                        ),
+                        onChanged: (value){
+                          txt = value;
+                          setState(() {
+                          });
+                        },
+                        validator: (value){
+                          if(value.isEmpty){
+                            return "Please enter username";
+                          }
 
-                    InkWell(
-                      onTap: () async {
-                        setState(() {
-                          changeButton = true;
-                        });
-
-                        await Future.delayed(Duration(seconds: 1));
-                        Navigator.pushNamed(context, MyRoutes.homeRoute);
-                      },
-                      child: AnimatedContainer(
-                        duration:Duration(seconds:1),
-                        width:changeButton?50 : 150,
-                        height: 45,
-                        alignment: Alignment.center,
-                        child: changeButton?Icon(Icons.done,color: Colors.white,):Text("Login",style:
-                        TextStyle(color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16),),
-                        decoration: BoxDecoration(
-                          color: Colors.deepPurple,
-                          borderRadius: BorderRadius.circular(changeButton?50:8)),
+                          return null;
+                        },
                       ),
-                    )
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Please enter password",
+                          labelText: "Password",
+                        ),
+                        validator: (value){
+                          if(value.isEmpty){
+                            return "Please enter password";
+                          }else if(value.length<6){
+                            return "Password length should be more than 6 character";
+                          }
 
-                    /*ElevatedButton(
-                      child: Text("Login"),
-                      style: TextButton.styleFrom(minimumSize: Size(140, 40)),
-                      onPressed: () {
-                     Navigator.pushNamed(context, MyRoutes.homeRoute);
-                      },
-                    )*/
-                  ],
+                          return null;
+                        },
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+
+                      InkWell(
+                        onTap: () => moveToHomepage(),
+                        child: AnimatedContainer(
+                          duration:Duration(seconds:1),
+                          width:changeButton?50 : 150,
+                          height: 45,
+                          alignment: Alignment.center,
+                          child: changeButton?Icon(Icons.done,color: Colors.white,):Text("Login",style:
+                          TextStyle(color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16),),
+                          decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            borderRadius: BorderRadius.circular(changeButton?50:8)),
+                        ),
+                      )
+
+                      /*ElevatedButton(
+                        child: Text("Login"),
+                        style: TextButton.styleFrom(minimumSize: Size(140, 40)),
+                        onPressed: () {
+                       Navigator.pushNamed(context, MyRoutes.homeRoute);
+                        },
+                      )*/
+                    ],
+                  ),
                 ),
               ),
             ],
