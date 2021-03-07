c49e0e6e667ec89e9e62be81f9c8bfa95c42077c
kunal kolhe <kunal.kolhe@webdunia.net>
2021-03-07 23:59:41 +0530

    Get json data from local json files. Day13 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catelog/model/catelog.dart';
 import 'file:///C:/Users/diaspark/FlutterProject/flutter_catelog/lib/widgets/mydrawer.dart';
 import 'package:flutter_catelog/widgets/ItemWidget.dart';
+import 'dart:convert';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
   List dummyList = List.generate(50, (index) => CatelogModel.items[0]);
+  List dataList = new List<Item>();
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
   @override
   Widget build(BuildContext context) {
     final int days = 1;
@@ -15,10 +29,20 @@ class Homepage extends StatelessWidget {
         body: ListView.builder(
           itemCount: dummyList.length,
           itemBuilder : (context,index){
-            return ItemWidget(item: dummyList[index],);
+            return ItemWidget(item: dummyList [index],);
           }
         ),
         drawer: MyDrawer(),
       );
+  }
+
+  Future<void> loadData() async {
+      var jsonFileData = await rootBundle.loadString("assets/files/catelog.json");
+      var catelogData = jsonDecode(jsonFileData);
+      //var items = catelogData("products");
+    //  print(items);
+    /*  dataList.addAll(items);
+      print(dataList);
+      print(dataList.length);*/
   }
 }
