eb18a7eaf500501b3c8414c8b9fb4a3463bdddda
kunal kolhe <kunal.kolhe@webdunia.net>
2021-03-09 00:00:44 +0530

    Grid View Day16 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_catelog/model/Product.dart';
@@ -25,12 +26,28 @@ class _HomepageState extends State<Homepage> {
     return Scaffold(
       appBar: AppBar(title: Text("Catelog"),
       centerTitle: true,),
-        body: (productList !=null && productList.isNotEmpty)? ListView.builder(
-          itemCount: productList.length,
-          itemBuilder : (context,index){
-            return ItemWidget(item: productList [index],);
-          }
-        ):Center(
+        body: (productList !=null && productList.isNotEmpty)? GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,
+            mainAxisSpacing: 16,
+            crossAxisSpacing: 10),
+             itemCount: productList.length,
+       itemBuilder: (context,index){
+               final item = productList[index];
+              return Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                clipBehavior: Clip.antiAlias,
+                child: GridTile(child: Image.network(item.image),
+                header: Container(child: Center(child: Text(item.name,style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 16),)),
+                color: Colors.deepPurple,
+                padding: EdgeInsets.all(10),),
+                footer: Container(child: Center(child: Text(item.price,style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 16),)),
+         color: Colors.black,
+         padding: EdgeInsets.all(10),),
+              ));
+       }
+         ):Center(
           child: CircularProgressIndicator(),
         ),
         drawer: MyDrawer(),
