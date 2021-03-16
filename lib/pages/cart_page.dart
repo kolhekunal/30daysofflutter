afb3d9e38d61825b8ad472b6d750e8d398ab050d
kunal kolhe <kunal.kolhe@webdunia.net>
2021-03-16 16:12:16 +0530

    Cart Page Design Day22 Completed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -11,6 +11,75 @@ class CartPage extends StatelessWidget {
            centerTitle:true,
            title: "Cart List".text.capitalize.blue700.make(),
          ),
+       body: Column(
+         children: [
+           _cartItemList().p16().expand(),
+           Divider(),
+           _cartList()
+
+         ],
+
+
+       ),
      );
    }
  }
+
+
+
+ class _cartList extends StatefulWidget {
+   @override
+   __cartListState createState() => __cartListState();
+ }
+
+ class __cartListState extends State<_cartList> {
+   @override
+   Widget build(BuildContext context) {
+     return SizedBox(
+       height: 200,
+       child: Row(
+         mainAxisAlignment: MainAxisAlignment.spaceAround ,
+         children: [
+           "\$9999".text.xl3.make(),
+           30.widthBox,
+           ElevatedButton(
+             onPressed:(){},
+             style:ButtonStyle(backgroundColor: MaterialStateProperty.all(context.theme.buttonColor)),
+             child: "Buy".text.center.make().w20(context),
+
+           )
+
+         ],
+
+       ),
+
+
+     );
+   }
+ }
+
+
+
+ class _cartItemList extends StatefulWidget {
+   @override
+   __cartItemListState createState() => __cartItemListState();
+ }
+
+ class __cartItemListState extends State<_cartItemList> {
+   @override
+   Widget build(BuildContext context) {
+     return ListView.builder(
+       itemCount: 5,
+         itemBuilder: (context,index) => ListTile(
+           leading: Icon(Icons.done),
+           trailing: IconButton(
+             icon: Icon(Icons.remove_circle_rounded),
+             onPressed: (){},
+           ),
+           title: "Item_1".text.center.make(),
+
+         ));
+   }
+ }
+
+
